23746ff1ab46dd38b8d40bf2ed54e99ff2fcaa5b
Edward.M <99274690+Eddie-Mcnamara@users.noreply.github.com>
2022-04-26 15:05:34 +0000

    added linter

---
+++ js/script.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2022 Edward Mcnamara All rights reserved
+//
+// Created by: Edward Mcnamara
+// Created on: March 2022
+// This file contains the JS functions for index.html
+
+'use strict';
+/**
+ * This function calculates the volume of a sphere given its radius
+ */
+function calculate() {
+  // Entering radius in cm
+  let radius = parseFloat(document.getElementById('radius').value)
+  let height = parseFloat(document.getElementById('height').value)
+  let length = parseFloat(document.getElementById('length').value)
+
+
+  // calculating volume using formula (Note: I was unable to get math.pi working properly)
+  // const pi = 3.141592653589793238
+  let volume = (4.0/3.0) * Math.PI * radius**3
+
+
+  // Displaying volume results  
+  document.getElementById('volume').innerHTML = 'The volume of the circle is ' + volume.toFixed(2) + 'cm<sup>3</sup>'
+  
